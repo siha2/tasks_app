c650ecb0611e83d3f733646c6d3bf2845401770a
siha <sihaemad55@gmail.com>
2023-10-10 19:48:58 +0300

    upload files

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # tasks_app
-To Do Application
+To Do Application [tasks_app](https://siha2.github.io/tasks_app/)
---
+++ main.js
@@ -0,0 +1,93 @@
+let input = document.querySelector(".input");
+let submit = document.querySelector(".add");
+let tasksDiv = document.querySelector(".tasks");
+let arrayOfTasks = [];
+
+if (localStorage.getItem("tasks")) {
+  arrayOfTasks = JSON.parse(localStorage.getItem("tasks"));
+}
+
+getDataFromLocalStorage();
+
+// add task
+submit.onclick = function () {
+  if (input.value !== "") {
+    addTaskToArray(input.value);
+    input.value = "";
+  }
+}
+
+tasksDiv.addEventListener("click", (e) => {
+  // delete button
+  if (e.target.classList.contains("del")) {
+    // remove task from local storage
+    deleteTaskWith(e.target.parentElement.getAttribute("data-id"));
+    // remove element from page
+    e.target.parentElement.remove();
+  }
+  // task element
+  if (e.target.classList.contains("task")) {
+    // toggle completed for the task
+    toggleStatusTaskWith(e.target.getAttribute("data-id"));
+    // toggle done class
+    e.target.classList.toggle("done");
+  }
+})
+
+function addTaskToArray(taskText) {
+  const task = {
+    id: Date.now(),
+    title: taskText,
+    completed: false
+  };
+  arrayOfTasks.push(task);
+  addElementsToPageFrom(arrayOfTasks);
+  addDataToLocalStorageFrom(arrayOfTasks);
+}
+
+function addElementsToPageFrom(arrayOfTasks) {
+  // empty tasks div
+  tasksDiv.innerHTML = "";
+  arrayOfTasks.forEach((task) => {
+    // create main div
+    let div = document.createElement("div");
+    div.className = "task";
+    if (task.completed) {
+      div.className = "task done";
+    }
+    div.setAttribute("data-id", task.id);
+    div.appendChild(document.createTextNode(task.title));
+    // create delete button
+    let span = document.createElement("span");
+    span.className = "del";
+    span.appendChild(document.createTextNode("Delete"));
+    div.appendChild(span);
+    tasksDiv.appendChild(div);
+  });
+}
+
+function addDataToLocalStorageFrom(arrayOfTasks) {
+  localStorage.setItem("tasks", JSON.stringify(arrayOfTasks));
+}
+
+function getDataFromLocalStorage() {
+  let data = localStorage.getItem("tasks");
+  if (data) {
+    let tasks = JSON.parse(data);
+    addElementsToPageFrom(tasks);
+  }
+}
+
+function deleteTaskWith(taskId) {
+  arrayOfTasks = arrayOfTasks.filter((task) => task.id != taskId);
+  addDataToLocalStorageFrom(arrayOfTasks);
+}
+
+function toggleStatusTaskWith(taskId) {
+  for (let i = 0; i < arrayOfTasks.length; i++) {
+    if (arrayOfTasks[i].id == taskId) {
+      arrayOfTasks[i].completed == false ? arrayOfTasks[i].completed = true : arrayOfTasks[i].completed = false;
+    }
+  }
+  addDataToLocalStorageFrom(arrayOfTasks);
+}
